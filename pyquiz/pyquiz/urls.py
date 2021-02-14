aee69842caa9f5cc60a2cf04ca6ca040400d26e5
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-14 19:42:14 +0900

    Added comment

--- pyquiz/pyquiz/urls.py
+++ pyquiz/pyquiz/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # Added the blog path to the projects urls to look for
     path("", include("blog.urls")),
 ]
