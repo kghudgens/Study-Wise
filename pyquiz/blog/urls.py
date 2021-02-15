9d850260edca033ec1b4d279caaca39d540663bd
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-15 21:56:01 +0900

    list view for the blog

--- pyquiz/blog/urls.py
+++ pyquiz/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="index"),
+    path("blog/", views.BlogList.as_view(), name="blog-list"),
 ]
