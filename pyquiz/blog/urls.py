92b203fa346153a0630c9031337e50dd6dfc0d0a
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-16 19:40:08 +0900

    corrected name on path

--- pyquiz/blog/urls.py
+++ pyquiz/blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("about/", views.about, name="index"),
+    path("about/", views.about, name="about"),
     path("blog/", views.BlogList.as_view(), name="blog-list"),
 ]
