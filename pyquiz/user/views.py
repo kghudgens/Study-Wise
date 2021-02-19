2c7b137a599b8bc34d03d1c4af0c93c2963f666d
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-19 23:40:35 +0900

    created view to handle register form

--- pyquiz/user/views.py
+++ pyquiz/user/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+
 from .forms import PyQuizUserForm
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "user/register.html", {"form": form})
