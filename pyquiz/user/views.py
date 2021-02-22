d1a5a9224a0229cbd59d3396d11d5cba8052fa81
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-22 22:47:51 +0900

    created view for profile and added messages

--- pyquiz/user/views.py
+++ pyquiz/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 from .forms import PyQuizUserForm
 
@@ -10,7 +11,12 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Your account has been created.")
             return redirect("login")
     else:
         form = UserCreationForm()
     return render(request, "user/register.html", {"form": form})
+
+
+def profile(request):
+    return render(request, "user/profile.html")
