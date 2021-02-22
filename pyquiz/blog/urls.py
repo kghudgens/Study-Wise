100a46d320d670b4e73c7a3a6205681bd00ebff5
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-22 22:38:29 +0900

    put url for register view in correct file

--- pyquiz/blog/urls.py
+++ pyquiz/blog/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
-from user import views as user_views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
     path("blog/", views.BlogList.as_view(), name="blog-list"),
-    path("register/", user_views.register, name="register"),
 ]
