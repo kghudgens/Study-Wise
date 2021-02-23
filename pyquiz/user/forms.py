6000aafa27ec1d997fad49a8f2bc9791ef5a58cc
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-23 23:15:26 +0900

    created the update user form

--- pyquiz/user/forms.py
+++ pyquiz/user/forms.py
@@ -1,16 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 
 
-class PyQuizUserForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, label="First Name")
-    last_name = forms.CharField(max_length=30, label="Last Name")
-
-    def save(self, commit=True):
-        user = super(PyQuizUserForm, self).save(commit=False)
-        user.first_name = first_name
-        user.last_name = last_name
-        if commit:
-            user.save()
-        return User
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "username", "email"]
