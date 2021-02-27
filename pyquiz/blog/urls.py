9926bd8d2f9d9ea094180dcf1a2b0f6f1e7c4f5c
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-28 00:43:19 +0900

    added the path for the create view

--- pyquiz/blog/urls.py
+++ pyquiz/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
     path("blog/", views.BlogList.as_view(), name="blog-list"),
+    path("create_post/", views.CreateView.as_view(), name="create"),
 ]
