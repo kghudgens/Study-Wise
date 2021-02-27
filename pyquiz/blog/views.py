6171fbd7d31520b05503bd8aa19d4d813a18870d
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-28 01:02:20 +0900

    overrode the form validation for the CBV

--- pyquiz/blog/views.py
+++ pyquiz/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import ListView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
 
@@ -16,7 +18,15 @@ class BlogList(ListView):
     template_name = "blog/blog.html"
 
 
-class CreateView(CreateView):
+class CreateView(LoginRequiredMixin, CreateView):
+    login_url = "/login/"
     model = Post
     template_name = "blog/post_form.html"
     fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("blog-list")
