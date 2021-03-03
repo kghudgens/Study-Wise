9ed08e8a3ab0dc16743c63add240990659146e09
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-03 22:22:48 +0900

    working with form visibility

--- pyquiz/blog/static/blog/script.js
+++ pyquiz/blog/static/blog/script.js
@@ -1,1 +1,20 @@
 'strict'
+// Select the profile div 
+const profileForm = document.querySelector('.profile');
+
+// Function that removes the display style to the form
+function showForm(){
+    profileForm.classList.remove('hidden');
+    // BUG
+    const closeBtn = document.querySelector('.closebtn')
+    closeBtn.addEventListener('click', function(){
+        profileForm.classList.add('.hidden')
+        }
+    )
+}
+
+// Selects the button and store in variable
+const updateProfileBtn = document.getElementById('formbtn');
+
+// stored button now using the showform function on any click action
+updateProfileBtn.addEventListener('click', showForm);
