75a1e6fbe45728add4b11aab072c75fe8cd06ca7
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-03 17:27:05 +0900

    Created the comment model

--- pyquiz/blog/models.py
+++ pyquiz/blog/models.py
@@ -15,3 +15,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    comment = models.ForeignKey(Post, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
