f5fe242c2080d60f71e6f89feb2d008b0bda82b4
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-11 00:29:56 +0900

    included the quiz app urls

--- pyquiz/pyquiz/urls.py
+++ pyquiz/pyquiz/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # Added the blog path to the projects urls to look for
     path("", include("blog.urls")),
+    path("quiz/", include("quiz.urls")),
     path("register/", user_views.register, name="register"),
     path("accounts/", include("django.contrib.auth.urls")),
     path(
