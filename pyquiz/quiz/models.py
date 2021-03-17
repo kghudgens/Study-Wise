e764f4b3ca0069f10838a4607fdbd0846fc9271a
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-17 22:23:22 +0900

    Created the quiz model

--- pyquiz/quiz/models.py
+++ pyquiz/quiz/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Quiz(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    subject = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
