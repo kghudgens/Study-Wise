a1488fac9ad0935f9634a7b258a00f01dfa6318f
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-17 22:46:58 +0900

    routed the listviews url

--- pyquiz/quiz/urls.py
+++ pyquiz/quiz/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     # Correct this to reflect the quiz app
-    path("quiz/", views.quiz, name="quiz"),
+    path("quiz/", views.QuizListView.as_view(), name="quiz-list"),
 ]
