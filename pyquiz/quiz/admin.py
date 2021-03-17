cd2a0bc2f61709133ba5f53c8f389d63f67959f4
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-17 22:41:40 +0900

    Registered the models to use in admin

--- pyquiz/quiz/admin.py
+++ pyquiz/quiz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import QuestionAnswer, Quiz, QuizQuestions
 
 # Register your models here.
+admin.site.register(QuestionAnswer)
+admin.site.register(Quiz)
+admin.site.register(QuizQuestions)
