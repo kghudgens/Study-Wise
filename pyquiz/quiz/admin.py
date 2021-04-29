fd2f73a27472c9b4a11433b0606507d2d8e7bdef
Kevin Hudgens <hudgens1073@gmail.com>
2021-04-29 23:26:34 +0900

    changed direction of app

--- pyquiz/quiz/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import QuestionAnswer, Quiz, QuizQuestions
-
-# Register your models here.
-admin.site.register(QuestionAnswer)
-admin.site.register(Quiz)
-admin.site.register(QuizQuestions)
--- pyquiz/quiz/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("quiz/", views.QuizListView.as_view(), name="quiz-list"),
-    path("create_new_quiz", views.createQuiz, name="create-quiz-form"),
-]
