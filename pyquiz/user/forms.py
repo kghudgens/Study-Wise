f03ad744a491253ad82f513057567db2e13a3057
Kevin Hudgens <hudgens1073@gmail.com>
2021-04-30 10:10:38 +0900

    seperated user and profile form

--- pyquiz/user/forms.py
+++ pyquiz/user/forms.py
@@ -1,8 +1,24 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["first_name", "last_name", "username", "email"]
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        ]
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            "subjectOfInterest",
+            "goal",
+            "age",
+        ]
