845c6c149c9fd8080d9f6ace658ae1b4635952cb
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-01 18:41:25 +0900

    removed the default argument from the fields

--- pyquiz/user/models.py
+++ pyquiz/user/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    subjectOfInterest = models.CharField(max_length=100, default="Default String")
-    age = models.IntegerField(default=18)
-    goal = models.TextField(max_length=500, default="Default String")
+    subjectOfInterest = models.CharField(max_length=100)
+    age = models.IntegerField()
+    goal = models.TextField(max_length=500)
 
     def __str__(self):
         return f"Username: {self.user}"
