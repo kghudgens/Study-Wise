962b4bd6c6b5677560aba59eb4da098290114b58
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-04 22:52:27 +0900

    removed the default strings

--- pyquiz/user/models.py
+++ pyquiz/user/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subjectOfInterest = models.CharField(max_length=100)
+    age = models.IntegerField()
+    goal = models.TextField(max_length=500)
 
     def __str__(self):
         return f"Username: {self.user}"
