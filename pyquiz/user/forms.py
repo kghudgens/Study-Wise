68128b75b6288b86c097a643bca7d6c4b15c7a45
Kevin Hudgens <hudgens1073@gmail.com>
2021-05-08 19:02:19 +0900

    put labels on user form

--- pyquiz/user/forms.py
+++ pyquiz/user/forms.py
@@ -6,6 +6,12 @@ from .models import Profile
 class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
+        labels = {
+            "first_name": "First Name",
+            "last_name": "Last Name",
+            "username": "User Name",
+            "email": "Email",
+        }
         fields = [
             "first_name",
             "last_name",
@@ -17,6 +23,11 @@ class UpdateUserForm(forms.ModelForm):
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
+        labels = {
+            "subjectOfInterest": "What subject interests you most?",
+            "goal": "Whats the goal you want to achieve with Study Wise?",
+            "age": "What is your age?",
+        }
         fields = [
             "subjectOfInterest",
             "goal",
