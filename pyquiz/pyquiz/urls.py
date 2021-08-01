14a942597a78dc770155e66daba6c3f34ccf1c63
Kevin Hudgens <hudgens1073@gmail.com>
2021-08-01 22:48:51 +0900

    set settings if project still in debug

--- pyquiz/pyquiz/urls.py
+++ pyquiz/pyquiz/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from user import views as user_views
 
 
@@ -22,3 +24,6 @@ urlpatterns = [
     ),
     path("profile", user_views.profile, name="profile"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
