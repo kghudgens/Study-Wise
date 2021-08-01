db3d7052d9a3f57d7620a247edafa0eac41f00b4
Kevin Hudgens <hudgens1073@gmail.com>
2021-08-01 23:15:09 +0900

    registered the new model

--- pyquiz/blog/admin.py
+++ pyquiz/blog/admin.py
@@ -4,4 +4,4 @@ from .models import Post, Comment, PostImage
 # display these models on the admin page
 admin.site.register(Post)
 admin.site.register(Comment)
-admin.stie.register(PostImage)
+admin.site.register(PostImage)
