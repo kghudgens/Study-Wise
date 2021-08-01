dc3cb0c339e5387e44a757b555c0b6727cc39298
Kevin Hudgens <hudgens1073@gmail.com>
2021-08-01 23:09:12 +0900

    register the post image model

--- pyquiz/blog/admin.py
+++ pyquiz/blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, PostImage
 
+# display these models on the admin page
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.stie.register(PostImage)
