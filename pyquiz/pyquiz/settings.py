2bbaea82c63722e047d8819b044cdf0996658436
Kevin Hudgens <hudgens1073@gmail.com>
2021-08-01 22:46:11 +0900

    added logic for find static media

--- pyquiz/pyquiz/settings.py
+++ pyquiz/pyquiz/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +132,8 @@ STATICFILES_DIRS = [
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = "profile"
+
+# Server path for to store files
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# reference url for browser to access the files over HTTP
+MEDIA_URL = "/media/"
