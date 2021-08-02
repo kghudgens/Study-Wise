3f0651ab46d6143bd0ac5ac508ede808dda6aa3b
Kevin Hudgens <hudgens1073@gmail.com>
2021-08-02 20:25:33 +0900

    Created the study post model

--- pyquiz/study/models.py
+++ pyquiz/study/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+# The choices for the subject field in the Study Post model
+subject_choices = (
+    ("Information Technology", "Information Technology"),
+    ("Math", "Math"),
+    ("Science", "Science"),
+    ("History", "History"),
+    ("Literature", "Literature"),
+    ("Foreign Language", "Foreign Language"),
+    ("Geography", "Geography"),
+)
+
+
+class StudyPost(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    subject = models.CharField(
+        max_length=30, choices=subject_choices, default="Information Technology"
+    )
+    content = models.TextField()
+    photo = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return self.title
