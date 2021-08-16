cad1e7c16f023c0293d6a43beecfedaed57c50f0
Kevin Hudgens <hudgens1073@gmail.com>
2021-08-16 19:06:52 +0900

    created path for the create view

--- pyquiz/study/urls.py
+++ pyquiz/study/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("study/", views.study, name="study")]
+urlpatterns = [
+    path("study/", views.study, name="study"),
+    path(
+        "new_study_post/",
+        views.StudyPostCreateView.as_view(template_name="study/studypost_form.html"),
+        name="create-guide",
+    ),
+]
