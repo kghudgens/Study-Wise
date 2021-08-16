30abf6b2f1f82ece9aee0d5fa404ed7b1df69576
Kevin Hudgens <hudgens1073@gmail.com>
2021-08-16 19:06:37 +0900

    Created create view for study guides

--- pyquiz/study/views.py
+++ pyquiz/study/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import StudyPost
 
 
 def study(request):
     return render(request, "study/study.html")
+
+
+class StudyPostCreateView(CreateView):
+    model = StudyPost
+    fields = ["title", "subject", "photo", "content"]
