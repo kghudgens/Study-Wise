693dc67cfc494cbb931a2f2cd823b0395fb9aa41
Kevin Hudgens <hudgens1073@gmail.com>
2021-09-15 16:09:22 +0900

    added absolute url

--- pyquiz/study/models.py
+++ pyquiz/study/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 from PIL import Image
 
@@ -27,3 +28,6 @@ class StudyPost(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("studypost_detail", kwargs={"pk": self.pk})
