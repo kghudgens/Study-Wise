629e11765faa5762df7d1d2f7e96498ecdf7b209
Kevin Hudgens <hudgens1073@gmail.com>
2021-09-15 16:08:33 +0900

    Created detail view for the study post

--- pyquiz/study/views.py
+++ pyquiz/study/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import CreateView
+from django.views.generic import DetailView, CreateView
 from .models import StudyPost
 
 
 def study(request):
+
     study_guides = StudyPost.objects.all()[:3]
     context = {"study_guides": study_guides}
     return render(request, "study/study.html", context)
@@ -17,3 +18,9 @@ class StudyPostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class StudyPostDetailView(DetailView):
+    model = StudyPost
+    context_object_name = "sg"
+    template_name = "study/studypost_detail.html"
